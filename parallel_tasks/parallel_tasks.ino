211a19d835832604f989003cb3a60b266c3bf17c
dany3cunha <up201806651@fe.up.pt>
2022-11-08 23:03:03 +0000

    Update on send sms rotine
    Basically, the SMS() rotine only return when the sms was sent.

--- parallel_tasks/parallel_tasks.ino
+++ parallel_tasks/parallel_tasks.ino
@@ -58,7 +58,7 @@ RTC_DATA_ATTR int bootCount = 0;
 RTC_DATA_ATTR bool first_exec = true;
 
 // Mode Debug On/Off (print in Serial Monitor)
-// RTC_DATA_ATTR bool DBG_Mode = false;
+RTC_DATA_ATTR bool DBG_Mode = true;
 
 /** Vectors for saving the sensors readings, up to 10 measures.
     Vectors are clean when created a sms for sms_vec
@@ -85,8 +85,6 @@ bool T2_over = false;
 
 /*    Global Variables that don't need to be saved */
 
-// Mode Debug On/Off (print in Serial Monitor)
-bool DBG_Mode = true;
 /* ************************************************************ */
 
 bool setPowerBoostKeepOn(int en);
@@ -102,42 +100,41 @@ void setup()
   // Set serial for debug console (to Serial Monitor, default speed 115200)
   if (DBG_Mode)
     SerialMon.begin(115200);
-  /*
-    // Keep power when running from battery
-    Wire.begin(I2C_SDA, I2C_SCL);
-    bool isOk = setPowerBoostKeepOn(0);
-    if(DBG_Mode)
+
+  // Keep power when running from battery
+  Wire.begin(I2C_SDA, I2C_SCL);
+  bool isOk = setPowerBoostKeepOn(0);
+  if (DBG_Mode)
     SerialMon.println(String("IP5306 KeepOn ") + (isOk ? "OK" : "FAIL"));
-  */
 
   // Task 1 - Read Sensor Data, so works like the Master
   // create a task that will be executed in the Task1code() function, with priority 1 and executed on core 0
   xTaskCreatePinnedToCore(
-    Task1code, /* Task function. */
-    "Task1",   /* name of task. */
-    10000,     /* Stack size of task */
-    NULL,      /* parameter of the task */
-    1,         /* priority of the task */
-    &Task1,    /* Task handle to keep track of created task */
-    0);        /* pin task to core 0 */
+      Task1code, /* Task function. */
+      "Task1",   /* name of task. */
+      10000,     /* Stack size of task */
+      NULL,      /* parameter of the task */
+      1,         /* priority of the task */
+      &Task1,    /* Task handle to keep track of created task */
+      0);        /* pin task to core 0 */
   delay(500);
 
   // Task 2 - Send SMS when needed, so it's like a slave
   // create a task that will be executed in the Task2code() function, with priority 1 and executed on core 1
   xTaskCreatePinnedToCore(
-    Task2code, /* Task function. */
-    "Task2",   /* name of task. */
-    10000,     /* Stack size of task */
-    NULL,      /* parameter of the task */
-    1,         /* priority of the task */
-    &Task2,    /* Task handle to keep track of created task */
-    1);        /* pin task to core 1 */
+      Task2code, /* Task function. */
+      "Task2",   /* name of task. */
+      10000,     /* Stack size of task */
+      NULL,      /* parameter of the task */
+      1,         /* priority of the task */
+      &Task2,    /* Task handle to keep track of created task */
+      1);        /* pin task to core 1 */
   delay(500);
 
   if (DBG_Mode)
     SerialMon.println("Both tasks created!");
+  bootCount++;
 }
-
 
 void Task1code(void *pvParameters)
 {
@@ -145,7 +142,11 @@ void Task1code(void *pvParameters)
   // For controlling the execution time
   unsigned long start_t = millis();
 
-  String info = "Task1 running on core" + String(xPortGetCoreID());
+  if (DBG_Mode && bootCount == 1)
+  {
+    String info = "Task1 running on core" + String(xPortGetCoreID());
+    SerialMon.println(info);
+  }
 
   // Compare the size of sms_vec and pending_sms
   int sizeOf_smsVec = getSMS_vec_size();
@@ -157,13 +158,14 @@ void Task1code(void *pvParameters)
   }
 
   // Get Sensors readings (include) save values in the corresponding arrays
+  get_sensors_readings();
 
   measures_cnt++;
   if (DBG_Mode)
     SerialMon.println("(T1) measures_cnt: " + String(measures_cnt));
   if (measures_cnt == 10)
   {
-    // create SMS
+    // create SMS and save in the circular array
     create_SMS();
     pending_sms++;
     if (DBG_Mode)
@@ -179,19 +181,18 @@ void Task1code(void *pvParameters)
     unsigned long now = millis();
     if (T2_over || (now - start_t) > 55000)
     {
-      //break;
-      // Deep Sleep ESP
+      // break;
+      //  Deep Sleep ESP
       delay(100);
       unsigned long now = millis();
       // this 100ms is for the delay before deep_sleep_start()
       unsigned long TIME_TO_SLEEP = (start_t * 1000) + (60 * uS_TO_S_FACTOR) - (now * 1000) - (100 * 1000);
       esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP);
+      if (DBG_Mode)
+        SerialMon.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) + " uSeconds");
 
-      SerialMon.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) + " uSeconds");
-      // esp_deep_sleep_pd_config(ESP_PD_OPTION_OFF);
-      // SerialMon.println("Going to sleep now");
       delay(100);
-      // SerialMon.flush();
+
       esp_deep_sleep_start();
     }
     // Fundamental delay for the idle watchdog's that are intrinsic in ESP/Arduino
@@ -201,8 +202,6 @@ void Task1code(void *pvParameters)
 
     delay(10);
   }
-
-
 }
 
 void Task2code(void *pvParameters)
@@ -223,7 +222,8 @@ void Task2code(void *pvParameters)
               - in this case the MS can detect and ignore
   */
 
-  for (;;) {
+  for (;;)
+  {
 
     bool gsm_ON = false;
     while (pending_sms > 0)
@@ -235,10 +235,8 @@ void Task2code(void *pvParameters)
       }
 
       // Send SMS
-
       SMS();
-      // Clear the last SMS from sms_vec
-      // TO DO ...
+      // Clear the last SMS from sms_vec - TO DO !
 
       pending_sms--;
       if (DBG_Mode)
@@ -251,14 +249,15 @@ void Task2code(void *pvParameters)
       gsm_ON = false;
     }
 
-    if ( measures_cnt == 2)
-      delay(60000);
     T2_over = true;
     if (DBG_Mode)
       SerialMon.println("Task 2 finished !");
-    while (T2_over);
-  }
 
+    // This while only exits for busy waiting until the Task1 kills the Task2
+    // when going to sleep (to not allow Task2 to end)
+    while (T2_over)
+      ;
+  }
 }
 
 void loop()
@@ -270,116 +269,118 @@ bool setPowerBoostKeepOn(int en)
 {
   if (DBG_Mode)
     SerialMon.println("Hi from setPowerBoostKeepOn !");
-  /*
-    Wire.beginTransmission(IP5306_ADDR);
-    Wire.write(IP5306_REG_SYS_CTL0);
-    if (en)
-    {
+
+  Wire.beginTransmission(IP5306_ADDR);
+  Wire.write(IP5306_REG_SYS_CTL0);
+  if (en)
+  {
     Wire.write(0x37); // Set bit1: 1 enable 0 disable boost keep on
-    }
-    else
-    {
+  }
+  else
+  {
     Wire.write(0x35); // 0x37 is default reg value
-    }
-    return Wire.endTransmission() == 0;
-  */
+  }
+  return Wire.endTransmission() == 0;
 }
 void wake_up_gsm()
 {
   if (DBG_Mode)
     SerialMon.println("Hi from wake_up_gsm !");
-  /*
-    unsigned long start_t = millis();
 
-    // Set modem reset, enable, power pins
-    pinMode(MODEM_PWKEY, OUTPUT);
-    pinMode(MODEM_RST, OUTPUT);
-    pinMode(MODEM_POWER_ON, OUTPUT);
+  // Set modem reset, enable, power pins
+  pinMode(MODEM_PWKEY, OUTPUT);
+  pinMode(MODEM_RST, OUTPUT);
+  pinMode(MODEM_POWER_ON, OUTPUT);
 
+  // USELESS IN THE v1.3
+  // pinMode(MODEM_DTR, OUTPUT);
+  // digitalWrite(MODEM_DTR, HIGH);
 
-    //USELESS IN THE v1.3
-    //pinMode(MODEM_DTR, OUTPUT);
-    //digitalWrite(MODEM_DTR, HIGH);
+  digitalWrite(MODEM_PWKEY, LOW);
+  digitalWrite(MODEM_RST, HIGH);
+  digitalWrite(MODEM_POWER_ON, HIGH);
 
-    digitalWrite(MODEM_PWKEY, LOW);
-    digitalWrite(MODEM_RST, HIGH);
-    digitalWrite(MODEM_POWER_ON, HIGH);
+  // Restart SIM800 module, it takes quite some time
+  // To skip it, call init() instead of restart()
 
-    // Restart SIM800 module, it takes quite some time
-    // To skip it, call init() instead of restart()
-
-    // Set GSM module baud rate and UART pins
-    SerialAT.begin(115200, SERIAL_8N1, MODEM_RX, MODEM_TX);
-    //// atention to this delay
-    delay(3000);
-    /////////////////////////////
-    if (DBG_Mode)
+  // Set GSM module baud rate and UART pins
+  SerialAT.begin(115200, SERIAL_8N1, MODEM_RX, MODEM_TX);
+  //// atention to this delay
+  delay(3000);
+  /////////////////////////////
+  if (DBG_Mode)
     SerialMon.println("Initializing modem...");
-    modem.init();
-    unsigned long mid1 = millis();
-    // use modem.init() if you don't need the complete restart
-    if (DBG_Mode)
-    SerialMon.println("From begin to modem.init ends: " + String(mid1 - start_t));
-    // Unlock your SIM card with a PIN if needed
-    if (strlen(simPIN) && modem.getSimStatus() != 3)
-    {
-    modem.simUnlock(simPIN);
-    }
+  modem.init();
+  // use modem.init() if you don't need the complete restart
 
-    // int csq = modem.getSignalQuality();
-    // SerialMon.println("Signal quality: " + String(csq));
-  */
+  // Unlock your SIM card with a PIN if needed
+  if (strlen(simPIN) && modem.getSimStatus() != 3)
+  {
+    modem.simUnlock(simPIN);
+  }
+
+  if (DBG_Mode)
+  {
+    csq = modem.getSignalQuality();
+    SerialMon.println("Signal quality: " + String(csq));
+  }
 }
 void shutdown_gsm()
 {
   if (DBG_Mode)
     SerialMon.println("Hi from shutdown_gsm !");
 
-  /*
-    digitalWrite(MODEM_PWKEY, HIGH);
-    delay(50);
-    digitalWrite(MODEM_PWKEY, LOW);
-    delay(1550);
-    digitalWrite(MODEM_PWKEY, HIGH);
-  */
+  digitalWrite(MODEM_PWKEY, HIGH);
+  delay(50);
+  digitalWrite(MODEM_PWKEY, LOW);
+  delay(1550);
+  digitalWrite(MODEM_PWKEY, HIGH);
 }
 void SMS()
 {
   if (DBG_Mode)
-    SerialMon.println("Hi from SMS !");
-  /*
-    bool res;
-    res = modem.testAT();
-    if (res)
-    {
+    SerialMon.println("(cbSMS)" + msg + " !");
+
+  bool res;
+  res = modem.testAT();
+  if (res)
+  {
     if (DBG_Mode)
-    SerialMon.println("GSM module is ON");
-    }
-    else
-    {
+      SerialMon.println("GSM module is ON");
+  }
+  else
+  {
     if (DBG_Mode)
-    SerialMon.println("GSM module is OFF");
+      SerialMon.println("GSM module is OFF");
     return;
-    }
+  }
 
-    unsigned long start_t = millis();
-    // To send an SMS, call modem.sendSMS(SMS_TARGET, smsMessage)
-    // String smsMessage = "Cycle: " + String(bootCount);
+  // unsigned long start_t = millis();
+  //  To send an SMS, call modem.sendSMS(SMS_TARGET, smsMessage)
+  //  String smsMessage = "Cycle: " + String(bootCount);
 
-    String time = modem.getGSMDateTime(DATE_FULL);
-    String smsMessage = time + "/TT.T/HHH/VV.VV";
-    // SerialMon.println(time);
-    if (modem.sendSMS(SMS_TARGET, smsMessage))
+  // String time = modem.getGSMDateTime(DATE_FULL);
+  // String smsMessage = time + "/TT.T/HHH/VV.VV";
+  //  SerialMon.println(time);
+  /*
+    if (modem.sendSMS(SMS_TARGET, msg))
     {
     // SerialMon.println(smsMessage);
-    if (DBG_Mode)
-    SerialMon.println("Send SMS time: " + String(millis() - start_t));
+    //if (DBG_Mode)
+    //  SerialMon.println("Send SMS time: " + String(millis() - start_t));
     }
     else
     {
-    // SerialMon.println("SMS failed to send");
-    }
-  */
+    if (DBG_Mode)
+      SerialMon.println("SMS failed to send");
+    }*/
+
+  while (!modem.sendSMS(SMS_TARGET, msg))
+  {
+    if (DBG_Mode)
+      SerialMon.println("SMS failed to send");
+    delay(500);
+  }
 }
 
 void get_sensors_readings()
@@ -387,14 +388,16 @@ void get_sensors_readings()
   if (DBG_Mode)
     SerialMon.println("Hi from get_sensors_readings !");
 
+  // Read Anenometer
   // Read DTH
-
 }
-void create_SMS() {
+void create_SMS()
+{
 }
 int getSMS_vec_size()
 {
   if (DBG_Mode)
     SerialMon.println("Hi from getSMS_vec_size (ret:0) !");
+  // should return the size of the circular array
   return 0;
 }
