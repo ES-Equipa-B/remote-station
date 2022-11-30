b53d69187d4a547ee627b098e90e15a8ac7e9ac2
dany3cunha <up201806651@fe.up.pt>
2022-11-30 01:35:20 +0000

    Changes in the format of sensor readings
    Some improvements in the format of the sensors readings for the SMS type

---
+++ remote_station_gsm_ino/main.hpp
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "sms.hpp"
+#include "sensors.hpp"
+#include "esp_sleep.h"
+
+#define SMS_TASK_TIMEOUT 55000 // miliseconds
+#define SMS_PER_HOUR 6
+
+#endif
